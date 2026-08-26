c97cd0593205b6f0d0543207bc19f3aaa1976fbd
Raul Huertas <rax20037@gmail.com>
2026-08-26 11:17:00 -0500

    Fullscreen by default

--- app/desktopApp/src/main/kotlin/com/rhuertas/cncjsdashboard/main.kt
+++ app/desktopApp/src/main/kotlin/com/rhuertas/cncjsdashboard/main.kt
@@ -1,10 +1,20 @@
 package com.rhuertas.cncjsdashboard
 
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPlacement
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 
-fun main() = application {
+fun main(args: Array<String>) = application {
+    val isWindowed = args.contains("--windowed")
+    val windowState = if (isWindowed) {
+        rememberWindowState()
+    } else {
+        rememberWindowState(placement = WindowPlacement.Fullscreen)
+    }
+
     Window(
+        state = windowState,
         onCloseRequest = ::exitApplication,
         title = "CNCJSDashboard",
     ) {
